5fbf0a3c02431bcdd13f123df15c8380c4a9cdc2
oliver-lloyd <ollielloyd96@gmail.com>
2020-06-27 11:25:01 +0100

    First commit: Increase-stats buttons are working

---
+++ Assets/Scripts/CreatureController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureController : MonoBehaviour
+{
+    public int stamina = 1;
+    public int staminaIncrease = 1;
+    public int agility = 1;
+    public int agilityIncrease = 1;
+    public int strength = 1;
+    public int strengthIncrease = 1;
+    public int speed = 1;
+    public int speedIncrease = 1;
+
+    public void Increment(string statToTrain)
+    {
+        Debug.Log($"Incrementing {statToTrain}");
+        if (statToTrain == "Stamina")
+        {
+            stamina += staminaIncrease;
+        }
+        else if (statToTrain == "Agility")
+        {
+            agility += agilityIncrease;
+        }
+        else if (statToTrain == "Strength")
+        {
+            strength += strengthIncrease;
+        }
+        else if (statToTrain == "Speed")
+        {
+            speed += speedIncrease;
+        }
+    }
+}
