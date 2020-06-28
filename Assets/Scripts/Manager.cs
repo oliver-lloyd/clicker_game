f2f69cc6abff51a4c554083b339123cd5260fc8b
oliver-lloyd <ollielloyd96@gmail.com>
2020-06-28 17:18:36 +0100

    Working counters and button text

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,16 +4,34 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
-
-
+using UnityEngine.UIElements;
 
 public class Manager : MonoBehaviour
 {
     public int coinCount = 1000;
     public Text wallet;
+    
+    public Text agilityCounter;
+    public Text speedCounter;
+    public Text staminaCounter;
+    public Text strengthCounter;
+    
+    public UnityEngine.UI.Button agilityUpgrade;
+    public UnityEngine.UI.Button speedUpgrade;
+    public UnityEngine.UI.Button staminaUpgrade;
+    public UnityEngine.UI.Button strengthUpgrade;
+
+    public UnityEngine.UI.Button agilityIncrement;
+    public UnityEngine.UI.Button speedIncrement;
+    public UnityEngine.UI.Button staminaIncrement;
+    public UnityEngine.UI.Button strengthIncrement;
+    
     public string[] statNames = { "Agility", "Speed", "Stamina", "Strength" };
-    public string[] statValues = { "XP", "Level", "IncrementAmount", "UpgradeCost" };
+    public string[] statValues = { "XP", "Level", "IncrementAmount", "UpgradeCost" , "Counter"};
+
     public Dictionary<string, Dictionary<string, int>> statDict = new Dictionary<string, Dictionary<string, int>>();
+    public Dictionary<string, Dictionary<string, UnityEngine.UI.Button>> buttonDict = new Dictionary<string, Dictionary<string, UnityEngine.UI.Button>>();
+    public Dictionary<string, Text> textDict = new Dictionary<string, Text>();
 
     private System.Random random = new System.Random();
     private int coinEventCount;
@@ -21,6 +39,13 @@ public class Manager : MonoBehaviour
 
     private void Start()
     {
+        textDict["Agility"] = agilityCounter;
+        textDict["Speed"] = speedCounter;
+        textDict["Stamina"] = staminaCounter;
+        textDict["Strength"] = strengthCounter;
+
+        
+
         //coinCount = GameObject.FindGameObjectWithTag("Wallet");
         UpdateCoins(0);
         foreach (string statName in statNames)
@@ -30,7 +55,33 @@ public class Manager : MonoBehaviour
             {
                 statDict[statName][statValue] = 1;
             }
+            buttonDict[statName] = new Dictionary<string, UnityEngine.UI.Button>();
+            textDict[statName].text = statName + $": {statDict[statName]["XP"]}";
+
+            
         }
+
+
+        buttonDict["Agility"]["Increment"] = agilityIncrement;
+        buttonDict["Agility"]["Upgrade"] = agilityUpgrade;
+        buttonDict["Agility"]["Increment"].GetComponentInChildren<Text>().text = $"Train Stamina! (+{statDict["Agility"]["IncrementAmount"]})";
+        buttonDict["Agility"]["Upgrade"].GetComponentInChildren<Text>().text = $"Buy {"Agility"} Upgrade (£{statDict["Agility"]["UpgradeCost"]})";
+
+        buttonDict["Speed"]["Increment"] = speedIncrement;
+        buttonDict["Speed"]["Upgrade"] = speedUpgrade;
+        buttonDict["Speed"]["Increment"].GetComponentInChildren<Text>().text = $"Train Stamina! (+{statDict["Speed"]["IncrementAmount"]})";
+        buttonDict["Speed"]["Upgrade"].GetComponentInChildren<Text>().text = $"Buy {"Speed"} Upgrade (£{statDict["Speed"]["UpgradeCost"]})";
+        
+        buttonDict["Stamina"]["Increment"] = staminaIncrement;
+        buttonDict["Stamina"]["Upgrade"] = staminaUpgrade;
+        buttonDict["Stamina"]["Increment"].GetComponentInChildren<Text>().text = $"Train Stamina! (+{statDict["Stamina"]["IncrementAmount"]})";
+        buttonDict["Stamina"]["Upgrade"].GetComponentInChildren<Text>().text = $"Buy {"Stamina"} Upgrade (£{statDict["Stamina"]["UpgradeCost"]})";
+        
+        buttonDict["Strength"]["Increment"] = strengthIncrement;
+        buttonDict["Strength"]["Upgrade"] = strengthUpgrade;
+        buttonDict["Strength"]["Increment"].GetComponentInChildren<Text>().text = $"Train Stamina! (+{statDict["Strength"]["IncrementAmount"]})";
+        buttonDict["Strength"]["Upgrade"].GetComponentInChildren<Text>().text = $"Buy {"Strength"} Upgrade (£{statDict["Stamina"]["UpgradeCost"]})";
+
     }
     private void Update()
     {
@@ -46,10 +97,11 @@ public class Manager : MonoBehaviour
                 if (hit.transform.name == "Creature")
                 {
                     print("The creature is dancing!");
-                    // Play animation
+                    // Play animation here
                 }
             }
         }
+
     }
 
     private void FixedUpdate()
@@ -62,7 +114,9 @@ public class Manager : MonoBehaviour
         print($"Incrementing {statToTrain} by {statDict[statToTrain]["IncrementAmount"]}");
         
         statDict[statToTrain]["XP"] += statDict[statToTrain]["IncrementAmount"];
-        System.Threading.Thread.Sleep(1000);
+        textDict[statToTrain].text = statToTrain + $": {statDict[statToTrain]["XP"]}";
+
+        System.Threading.Thread.Sleep(500);
     }
     public void UpgradeStats(string statToUpgrade)
     {
@@ -76,6 +130,8 @@ public class Manager : MonoBehaviour
             statDict[statToUpgrade]["IncrementAmount"] = (int) Math.Pow(statDict[statToUpgrade]["Level"], 2);
             statDict[statToUpgrade]["UpgradeCost"] = (int) Math.Pow(statDict[statToUpgrade]["Level"], 3);
 
+            buttonDict[statToUpgrade]["Increment"].GetComponentInChildren<Text>().text = $"Train Stamina! (+{statDict[statToUpgrade]["IncrementAmount"]})";
+            buttonDict[statToUpgrade]["Upgrade"].GetComponentInChildren<Text>().text = $"Buy {statToUpgrade} Upgrade (£{statDict[statToUpgrade]["UpgradeCost"]})";
             Debug.Log($"Upgraded {statToUpgrade}, it will now give {statDict[statToUpgrade]["IncrementAmount"]} XP per click.");
         }
         else
@@ -93,7 +149,7 @@ public class Manager : MonoBehaviour
     
     public void RandomPickupCoins(float meanPeriod, int maxPickup)
     {
-        // 
+        
         float frequency = meanPeriod / Time.fixedDeltaTime;
 
         int numberOfAttempts = (int)frequency;
